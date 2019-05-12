567dad03de4cf29ae3c37f2714e7a9564ee99be4
ombaejr <ombaejr@gmail.com>
2019-05-12 10:15:44 +0300

    Write condition statement for adding the random number and fade effects

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,22 @@ function roll() {
   var totalScore = initScore += dice;
   output.unshift(score);
   console.log(output);
+
+  if (totalScore >= 100) {
+    $(document).ready(function() {
+      $("#player1").toggle(2000);
+      $("#player1").fadeOut(2000);
+      $("#win1").slideDown(2000);
+    })
+  } else {
+    if (dice == 1) {
+      $(document).ready(function() {
+        output.length = 0;
+        $("#player1").fadeOut(2000);
+        $("#alert1").fadeIn();
+      })
+    } else {
+      document.getElementById("roll1").innerHTML = totalScore;
+    }
+  }
+}
